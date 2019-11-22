38e6f9603d5a1740f672dcd5391102fac2b03917
pdoud2124 <57923049+pdoud2124@users.noreply.github.com>
2019-11-22 13:43:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+import psycopg2
+import psycopg2.extras
+from flask import Flask, render_template, request, g
+from random import randint
 app = Flask(__name__)
+
+IP_ADDR = "35.194.34.244"
 
 
 @app.route('/')
@@ -37,15 +42,61 @@ def view-item():
   return render_template("view-item.html")
 
 
-@app.route('/search-items')
-def search-items():
-  return render_template("search-items.html")
+@app.route("/search-item", methods=["get", "post"])
+def search():
+    if "query" not in request.form:
+        return render_template("search-item.html")
+    else:
+        db = get_db()
+        cursor = db.cursor()
+        query = str(request.form["query"])
+        sql_query = "select * from item_types natural join items natural join category" + \
+            " where LOWER(catname)=LOWER('" + query + "') or " + \
+            "LOWER(itemname) like LOWER(" + "'%" + query + "%') or " + \
+            "LOWER(description) like LOWER('%" + query + "%')"
+        print(sql_query)
+
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        return render_template("search-item.html", entries=rows)
 
 
 @app.route('/auctions')
 def auctions():
   return render_template("auctions.html")
 
+# ------------------------------------------
 
-if __name__ == "__main__":
-  app.run();
+# Database handling
+
+
+def connect_db():
+    """Connects to the database."""
+    debug("Connecting to DB.")
+    conn = psycopg2.connect(host=IP_ADDR, user="postgres", password="rhodes"
+        , dbname="exchangedb", cursor_factory=psycopg2.extras.DictCursor)
+    return conn
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'pg_db'):
+        g.pg_db = connect_db()
+    return g.pg_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database automatically when the application
+    context ends."""
+    debug("Disconnecting from DB.")
+    if hasattr(g, 'pg_db'):
+        g.pg_db.close()
+
+
+def debug(s):
+    """Prints a message to the screen if FLASK_DEBUG is set."""
+    if app.config['DEBUG']:
+        print(s)
