38e01b956cb07063e672c9ecc704002fb6bc30ad
pdoud2124 <57923049+pdoud2124@users.noreply.github.com>
2019-11-24 17:06:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import psycopg2
 import psycopg2.extras
+import datetime
+import time
 from flask import Flask, render_template, request, g
 from random import randint
 app = Flask(__name__)
@@ -9,7 +11,7 @@ IP_ADDR = "35.194.34.244"
 
 @app.route('/')
 def homepage():
-  return render_template("homepage.html")
+    return render_template("homepage.html")
 
 
 @app.route('/auctions', methods=['get', 'post'])
@@ -28,7 +30,7 @@ def auctions():
         #debug(row)
         if len(row) > 0:
             auctionid = row[0][0]
-            debug(auctionid)
+            #debug(auctionid)
             sql1 = "select * from bid where auctionid=" + str(auctionid)\
                 + " Order by bidprice"
             #debug(sql1)
@@ -36,17 +38,17 @@ def auctions():
             bids = cursor1.fetchall()
             finalprice = bids[len(bids)-1][2]
             sql3 = "select * from chat where auctionid=" + str(auctionid)
-            debug(sql3)
+            #debug(sql3)
             cursor3.execute(sql3)
             chats = cursor3.fetchall()
-            debug(bids)
+            #debug(bids)
             if "step" not in request.form:
                 return render_template("auctions.html", step="no bid", fp=finalprice, auction=row[0], bids=bids, chats=chats)
             elif request.form["step"] == "add_entry":
                 sql2 = "insert into bid(auctionId, bidder, bidPrice, bidTime) values ("\
                     + "'" + str(auctionid) + "'," + "'" + request.args.get("user") + "',"\
                     + "'" + request.form["bidamount"] + "', now())"
-                debug(sql2)
+                #debug(sql2)
                 cursor2.execute(sql2)
                 db.commit()
                 cursor1.execute(sql1)
@@ -57,7 +59,7 @@ def auctions():
                 sql4 = "insert into chat(chatText, chatTime, chatter, auctionId) values ("\
                     + "'" + request.form["chattext"] + "', now(), '"\
                     + request.args.get("user") + "','" + str(auctionid) + "'" + ")"
-                debug(sql4)
+                #debug(sql4)
                 cursor4.execute(sql4)
                 db.commit()
                 cursor3.execute(sql3)
@@ -67,65 +69,68 @@ def auctions():
             return render_template("auctions.html", item=itemid, auction=row)
     else:
         return render_template("auctions.html")
-      
 
-@app.route('/create-account' methods=["post"])
-def create-account():
-    if "username" not in request.form:
-        return render_template("create-account.html")
-    else if "email" not in request.form:
-        return render_template("create-account.html")
+
+@app.route('/create-account', methods=['get', 'post'])
+def create_account():
+    if 'account' not in request.form:
+        return render_template("create-account.html", account="notmade")
+    else:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("insert into users (email, username) values (%s, %s)",
+                   [request.form["email"], request.form["username"]])
+        db.commit()
+        return render_template("create-account.html", account="accountmade")
+
+
+@app.route('/sign-in', methods=['get', 'post'])
+def sign_in():
+    if 'credentials' not in request.form:
+        return render_template("sign-in.html", credentials="no")
     else:
         db = get_db()
         cursor = db.cursor()
         username = str(request.form["username"])
         email = str(request.form["email"])
-        cursor.execute("insert into users (email, username) values (%s, %s)",
-                   str(request.form["email"], str(request.form["username"]])
-        db.commit()
-        return render_template("create-account.html")
-
-
-@app.route('/sign-in')
-def sign-in():
-    if "username" not in request.form:
-        return render_template("sign-in.html")
-    else if "email" not in request.form:
-        return render_template("sign-in.html")
-    else:
-        db = get_db()
-        cursor = db.cursor()
-        username = str(request.form["username"])
-        username = str(request.form["email"])
         sql_query = "select * from users" + \
-            " where username='" + username + "') and " + \
+            " where username='" + username + "' and " + \
             "email like '" + email + "'"
-        print(sql_query)
+        debug(sql_query)
 
         cursor.execute(sql_query)
         user = cursor.fetchall()
-        print(user)
-        return render_template("search-item.html" user=user)
+        debug(user)
+        return render_template("sign-in.html", credentials="yes", name=user[0][1], email=user[0][0])
 
 
 @app.route('/user-home')
-def user-home():
+def user_home():
   return render_template("user-home.html")
 
 
-@app.route('/messages')
-def messages():
-  return render_template("messages.html")
+@app.route('/random', methods=['get', 'post'])
+def pick_number():
+    if "upperlim" not in request.form:
+        return render_template("random.html")
+    else:
+        upper = int(request.form['upperlim'])
+        n = randint(1, upper + 1)
+        return render_template("random.html", number=n)
 
 
-@app.route('/post-item')
-def post-item():
-  return render_template("post-item.html")
-
-
-@app.route('/view-item')
-def view-item():
-  return render_template("view-item.html")
+@app.route("/browse")
+def dump_entries():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('select * from users')
+    rows = cursor.fetchall()
+    output = ""
+    for r in rows:
+        debug(str(dict(r)))
+        output += str(dict(r))
+        output += "\n"
+    return "<pre>" + output + "</pre>"
 
 
 @app.route("/search-item", methods=["get", "post"])
@@ -140,16 +145,9 @@ def search():
             " where LOWER(catname)=LOWER('" + query + "') or " + \
             "LOWER(itemname) like LOWER(" + "'%" + query + "%') or " + \
             "LOWER(description) like LOWER('%" + query + "%')"
-        print(sql_query)
-
         cursor.execute(sql_query)
         rows = cursor.fetchall()
         return render_template("search-item.html", entries=rows)
-
-
-@app.route('/auctions')
-def auctions():
-  return render_template("auctions.html")
 
 # ------------------------------------------
 
